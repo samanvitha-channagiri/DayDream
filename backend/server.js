9f7e5f0dc9bae3571090ce364401239722a8701c
samanvitha-channagiri <samanvithachanna@gmail.com>
2025-04-19 20:25:22 +0530

    initial setup

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app=express();
+app.get('/',(req,res)=>{
+    res.send("Server is ready")
+})
+app.listen(8000,()=>{
+    console.log("server is running");
+    
+})
